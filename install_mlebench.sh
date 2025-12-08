f46e360932817c508284d59a5fd0262962cf17ff
rsdutta <duttars97@gmail.com>
2025-12-08 12:26:23 -0500

    Update commit hash

--- install_mlebench.sh
+++ install_mlebench.sh
@@ -3,7 +3,7 @@ set -e
 pushd src/dojo/tasks/mlebench
 git clone --depth 1 https://github.com/Mercor-Intelligence/meta-mle-task-creation-delivery.git
 pushd meta-mle-task-creation-delivery
-git fetch --depth 1 origin da193ffe4f92091d3d99637f412983e32f1602af
+git fetch --depth 1 origin 419fd36dcf83798d216670f3d11021dcb379ae2c
 git checkout FETCH_HEAD
 git lfs fetch --all
 git lfs pull
