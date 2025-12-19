01434a394d9cb84f75bf2a46f374d958dc1fd071
Jean-Philippe André <jeanphilippeandre@mercor.com>
2025-12-19 15:52:18 +0900

    update commit id

--- install_mlebench.sh
+++ install_mlebench.sh
@@ -3,7 +3,7 @@ set -e
 pushd src/dojo/tasks/mlebench
 git clone --depth 1 https://github.com/Mercor-Intelligence/meta-mle-task-creation-delivery.git
 pushd meta-mle-task-creation-delivery
-git fetch --depth 1 origin 26e3050c8371780e7c0031d9600ad99772eae6c5
+git fetch --depth 1 origin 7a59299673ede17076f7fcd64272defbd1155e70
 git checkout FETCH_HEAD
 git lfs fetch --all
 git lfs pull
